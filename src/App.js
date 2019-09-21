b5cca06b9ed7639d350c48bba2e61338a9aceb13
christiansallen <christiansallen@gmail.com>
2019-09-21 08:28:55 +0800

    changed some styling issues

--- src/App.js
+++ src/App.js
@@ -9,12 +9,14 @@ import AboutMe from "./AboutMe/AboutMe.js";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <TopBar />
-        <TopSection />
-        <MiddleSection1 />
-        <AboutMe />
-        <BottomSection />
+      <div className="app-background">
+        <div className="App">
+          <TopBar />
+          <TopSection />
+          <MiddleSection1 />
+          <AboutMe />
+          <BottomSection />
+        </div>
       </div>
     );
   }
